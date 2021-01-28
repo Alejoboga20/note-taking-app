ffb950e8cae01ccc9d9a8669d4c86a3f5920cbd1
alejoboga <alejoboga20@gmail.com>
2021-01-28 15:59:55 -0300

    Creating routes

--- src/routers/note.js
+++ src/routers/note.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 
+//CRUD Operations
+
+router.post('/notes', (req, res) => {});
+
+router.get('/notes', (req, res) => {});
+router.get('/notes/:noteId', (req, res) => {});
+
+router.patch('/notes/:noteId', (req, res) => {});
+
+router.delete('/notes/:noteId', (req, res) => {});
+
 module.exports = router;
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 
+//CRUD Operations
+
+router.post('/users', (req, res) => {});
+
+router.get('/users', (req, res) => {});
+router.get('/users/:userId', (req, res) => {});
+
+router.patch('/users/:userId', (req, res) => {});
+
+router.delete('/users/:userId', (req, res) => {});
+
 module.exports = router;
