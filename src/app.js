0b22a3289961467cb2be9fe61cbf38e737081e94
alejoboga <alejoboga20@gmail.com>
2021-01-28 15:33:39 -0300

    Instanciating Express Routers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const userRouter = require('./routers/user');
+const noteRouter = require('./routers/note');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(express.json());
+app.use(userRouter);
+app.use(noteRouter);
 
 app.listen(port, () => {
   console.log(`Server Up on port: ${port}`);
