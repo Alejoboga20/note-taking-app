b4eacf8867dafca4a9265011092c1b9bad842ff0
Alejo boga <alejoboga20@gmail.com>
2021-01-28 17:01:12 -0300

    Adding User Schema

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid');
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes('password')) {
+          throw new Error('Password can not contain "password"');
+        }
+      }
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
