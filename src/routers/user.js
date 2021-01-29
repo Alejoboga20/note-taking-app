cf73bd011bfffceb2e38bfaa9ee051696d26f29f
Alejo boga <alejoboga20@gmail.com>
2021-01-29 12:23:02 -0300

    Adding logout

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const e = require('express');
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 //CRUD Operations
@@ -25,6 +26,28 @@ router.post('/users/login', async (req, res) => {
     res.send({ user, token });
   } catch (e) {
     res.status(400).send();
+  }
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send('Loged out');
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send('Loged out All');
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
