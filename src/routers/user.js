6377de7d1f767af9d73a74218be91a69174e8328
Alejo boga <alejoboga20@gmail.com>
2021-01-29 10:52:44 -0300

    Adding create user request

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,9 +1,18 @@
+const e = require('express');
 const express = require('express');
+const User = require('../models/user');
 const router = new express.Router();
 
 //CRUD Operations
 
-router.post('/users', (req, res) => {});
+router.post('/users', async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {}
+});
 
 router.get('/users', (req, res) => {});
 router.get('/users/:userId', (req, res) => {});
