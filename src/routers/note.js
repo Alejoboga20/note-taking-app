5a7bb1d789357cc01274519b3a58149d2593080e
Alejo boga <alejoboga20@gmail.com>
2021-02-02 15:48:00 -0300

    updating Note

--- src/routers/note.js
+++ src/routers/note.js
@@ -40,7 +40,28 @@ router.get('/notes/:noteId', auth, async (req, res) => {
   }
 });
 
-router.patch('/notes/:noteId', (req, res) => {});
+router.patch('/notes/:noteId', auth, async (req, res) => {
+  const _id = req.params.noteId;
+  const allowedUpdates = ['title', 'body'];
+  const updates = Object.keys(req.body);
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid update' });
+  }
+
+  try {
+    const note = await Note.findOne({ _id, owner: req.user._id });
+    if (!note) {
+      return res.status(404).send();
+    }
+    updates.forEach((update) => (note[update] = req.body[update]));
+    await note.save();
+    res.send(note);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
 router.delete('/notes/:noteId', (req, res) => {});
 
