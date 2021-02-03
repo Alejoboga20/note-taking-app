e9157bddc171c855effadf499855c49725fca223
Alejo boga <alejoboga20@gmail.com>
2021-02-03 16:16:02 -0300

    Creating note test

---
+++ test/note.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const app = require('../src/app');
+const Note = require('../src/models/note');
+const User = require('../src/models/user');
+
+const userId = new mongoose.Types.ObjectId();
+const user = {
+  _id: userId,
+  name: 'TestNoteUser',
+  email: 'testnoteemail@test.com',
+  password: 'Testing123!',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userId }, process.env.JWT_SECRET)
+    }
+  ]
+};
+
+const newNote = {
+  title: 'Test Title',
+  body: 'Test Body'
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await Note.deleteMany();
+  await new User(user).save();
+});
+
+afterEach(async () => {
+  await User.deleteMany();
+});
+
+test('Should create a note', async () => {
+  const response = await request(app)
+    .post('/notes')
+    .set('Authorization', `Bearer ${user.tokens[0].token}`)
+    .send(newNote)
+    .expect(201);
+
+  console.log(response.body);
+});
